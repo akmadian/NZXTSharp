89d53279a50bbcd02f220dbdc3c8faab0e562b9d
Ari Madian <akmadian@gmail.com>
2019-05-05 15:49:16 -0700

    Add licence headers to all src files, update update-license-headers.py

--- Scripts/update-license-headers.py
+++ Scripts/update-license-headers.py
@@ -1,6 +1,25 @@
+"""
+update-license-headers.py adds license headers to all src files 
+that don't already have them
+Copyright (C) 2019  Ari Madian
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import os
 import re
-import datetime
+from datetime import datetime
 
 license_text = """/*
 {}
@@ -35,16 +54,17 @@ for root, dirs, files in os.walk('../NZXTSharp/'):
             continue
 
         split_name = name.split('.')
-        if split_name[-1] == 'cs':
+        if split_name[-1] == 'cs':  # If file extension is .cs
             src_content = ""
-            with open(os.path.join(root, name), 'r') as src: # get src, insert license
+            with open(os.path.join(root, name), 'r') as src:  # get src, insert license
                 oline = src.readlines()
                 if doesFileHaveLicenseText(oline):
                     print('File with license found -  {}... Skipping'.format(name))
                     continue
 
-                oline.insert(0, license_text.format(name, datetime.datetime().now().year))
+                formatted_licence_text = license_text.format(name, datetime.now().year)
+                oline.insert(0, formatted_licence_text)
                 src_content = oline
 
-            with open(os.path.join(root, name), 'w') as src: # overwrite file
+            with open(os.path.join(root, name), 'w') as src:  # overwrite file
                 src.writelines(src_content)
