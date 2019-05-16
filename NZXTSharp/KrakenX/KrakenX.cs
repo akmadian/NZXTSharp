c2973c4b379c59a72ec0049876b13ce02c65c774
Ari <akmadian@gmail.com>
2019-05-15 21:11:09 -0700

    Nothing of substance

--- NZXTSharp/KrakenX/KrakenX.cs
+++ NZXTSharp/KrakenX/KrakenX.cs
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
